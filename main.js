18d98f925fe2723dbbe7e461300d53cd6f24c3bb
mhmd <ms.mhmdsalah@gmail.com>
2024-02-04 18:51:04 +0200

    in progress

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// hello dear;
+
+
+function getQuestion () {
+  let myRequest = new XMLHttpRequest();
+  
+  myRequest.open( "GET", "html_questions.json", true )
+  myRequest.send()
+  myRequest.onreadystatechange = function() {
+    
+    if ( this.readyState === 4 && this.status === 200 ) {
+      console.log(this.response)
+    }
+    
+  }
+
+}
+getQuestion``
